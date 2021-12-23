81400170c49b91f13dab1007aff3a7a903e40655
urinboynaziraliyev <naziraliyevurinboy1996@gmail.com>
2021-12-23 21:09:28 +0500

    add rxJava

--- app/src/main/java/com/example/testappkotlin/MainActivity.kt
+++ app/src/main/java/com/example/testappkotlin/MainActivity.kt
@@ -21,9 +21,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         recyclerDataList.layoutManager = LinearLayoutManager(this)
         initApi()
-
         loadData()
-
     }
 
 
